fe7c3d5c9eb50e8462735920c98bacbe0b9297de
parallel <3359531715@qq.com>
2026-07-15 22:25:57 +0800

    数据结构

---
+++ data_struct/singly_linked_list.h
@@ -0,0 +1,12 @@
+#ifndef SINGLY_LINKED_LIST_H
+#define SINGLY_LINKED_LIST_H
+typedef enum status status;
+typedef enum state state;
+typedef struct List lst;
+lst* init_list(void);
+int get_data(lst L, int i, int e);
+lst* locate_list(lst L, int e);
+status insert(lst* L, int i, int elem);
+void createlist_head(lst* L, int n);
+void createlist_rear(lst* L, int n);
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "singly_linked_list.h"
+
+int main(){
+	return 0;
+}
+
